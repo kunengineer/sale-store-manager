a8d6a4ab9e87c6d8a400b97190848c6acf149187
trungkien <145746252+allpedog@users.noreply.github.com>
2026-02-21 22:22:25 +0700

    feat: Add Categories entity with relationships and attributes

---
+++ backend/src/main/java/com/be/ssm/entities/product/Categories.java
@@ -0,0 +1,45 @@
+package com.be.ssm.entities.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Categories {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Categories parent;
+
+    @Column(name = "category_name", nullable = false, length = 100)
+    private String categoryName;
+
+    @Column(name = "image_url", length = 255)
+    private String imageUrl;
+
+    @Column(name = "sort_order", nullable = false)
+    private Integer sortOrder;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
