93d084348b348ece92d7a224a7e2102640dcfd95
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2026-02-23 10:38:14 +0700

    feat(pom): add swagger test api

---
+++ backend/src/main/java/com/be/ssm/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.be.ssm.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes("bearerAuth",
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                                        .in(SecurityScheme.In.HEADER)
+//                        )
+//                )
+//              .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+                .info(new Info()
+                        .title("Human resource Management System API Documentation")
+                        .version("1.0")
+                        .description("API Documentation for HRM Project")
+                );
+    }
+}
