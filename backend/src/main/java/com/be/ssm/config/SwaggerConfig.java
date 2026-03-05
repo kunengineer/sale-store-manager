22d2966c85c27645256ba93f135dd9ffdc16ee8d
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2026-03-05 23:23:14 +0700

    feat(backend): add jwt beartoken on swagger

--- backend/src/main/java/com/be/ssm/config/SwaggerConfig.java
+++ backend/src/main/java/com/be/ssm/config/SwaggerConfig.java
@@ -12,16 +12,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-//                .components(new Components()
-//                        .addSecuritySchemes("bearerAuth",
-//                                new SecurityScheme()
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("bearer")
-//                                        .bearerFormat("JWT")
-//                                        .in(SecurityScheme.In.HEADER)
-//                        )
-//                )
-//              .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                        )
+                )
+              .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
                 .info(new Info()
                         .title("Human resource Management System API Documentation")
                         .version("1.0")
